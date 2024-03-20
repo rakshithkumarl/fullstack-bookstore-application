5c5bdac4fac8bd880a8e6d1914f532e295e38491
Rakshith Kumar Lokesh Rao <rakshithkumar.lr@gmail.com>
2024-03-19 17:12:25 -0700

    Configured the templates

--- bookstore/bookstore_project/bookstore_project/settings.py
+++ bookstore/bookstore_project/bookstore_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users_app.apps.UsersAppConfig'
+    'users_app.apps.UsersAppConfig',
+    'pages_app',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
